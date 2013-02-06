1760566a0dd1972cf774a7611e8be6711b9b9348
Ahmed Ismail <ahmedismail91@gmail.com>
2013-02-05 23:03:01 -0500

    Added comments

--- stereo2mono.c
+++ stereo2mono.c
@@ -17,9 +17,11 @@ int main(int argc, char *argv[])
     aFILE * f2 = fopen ("pianoSample.txt", "w"); // a text file that will have the left channel sound data,
                                                  //just to see what we're dealing with
     FILE * f3 = fopen ("pianoSample.csv", "w");  // an excel file that have the data to use in matLab
-    fseek (f , 0 , SEEK_END);
-    lSize = ftell (f);
-    rewind (f);
+    
+    /* Find size of wav file */
+    fseek (f , 0 , SEEK_END);   // set current position of the stream to the end of the file
+    lSize = ftell (f);      // number of bytes from the beginning of the file to the end
+    rewind (f);             // reset the current position of the file stream
     
     typedef struct wave {
                 int chunkID;
@@ -33,29 +35,28 @@ int main(int argc, char *argv[])
                 int bckAli_bitPs;
                 int subChunk2ID;
                 int subChunk2size;
-                int  data[(lSize-40)/4];
+                int  data[(lSize-40)/4];    // DOES THIS NEED TO BE lSize-44? The "/4" is because it's 1 word (32 bits) at a time
                 } wave;
     wave * mywave;
-    mywave=(wave *) malloc( lSize);
+    mywave=(wave *) malloc(lSize);
 
-    fread(mywave,1,lSize,f);
+    fread(mywave,1,lSize,f);            // load the whole file into the mywave buffer
 
 
     i=0,j=0;
-    int mask=65535;
-    int  leftVa;
-    while (i<((lSize-40)/4)){
-
-    leftVa=(mywave->data[i+1]<<16) | (mywave->data[i] & mask);
-    mywave->data[j]=leftVa;
-    fprintf(f2,"%x\n",  leftVa);
-    fprintf(f3,"%d\n",  leftVa);
-    j++;
-    i=i+2;
-                            }
+    int mask = 65535;   // 1111111111111111 -> 16 binary 1's
+    int leftVa;         // left channel parameter
+    while (i<((lSize-40)/4)){   // loop through each segment of data
+        leftVa = (mywave->data[i+1] << 16) | (mywave->data[i] & mask);  // mask out left channel value
+        mywave->data[j]=leftVa;     // store left channel data in mywave
+        fprintf(f2,"%x\n",  leftVa);    // Keep track of the sound data in hex, for viewing/debugging
+        fprintf(f3,"%d\n",  leftVa);    // Log decimal values for use in matlab
+        j++;
+        i=i+2;
+    }
 
     mywave->audio_num_ch=mywave->audio_num_ch-65536;
-    mywave->chunkSize=(mywave->chunkSize/2)+18;
+    mywave->chunkSize=(mywave->chunkSize/2)+18;         // Not sure about the '+18'
     mywave->byteRate=mywave->byteRate/2;
     mywave->bckAli_bitPs=mywave->bckAli_bitPs-2;
     mywave->subChunk2size=mywave->chunkSize-36;
