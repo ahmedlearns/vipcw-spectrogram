c3ee25fb3bd70b0aea9ba8a24950e08f4547929c
Ahmed Ismail <ahmedismail91@gmail.com>
2013-02-18 17:28:54 -0500

    Added a chirp, edited matlab code

--- specDisplay.m
+++ specDisplay.m
@@ -1,17 +1,17 @@
-/*
-	specDisplay.m
-
-	Takes the excel file containing the sound data file (720 chunks of 256 bytes).
-	Create an array of the fft of the data.
-	Output a text file containing the normalized fft to be used as an input to the video display code.
-
-*/
+% /*
+% 	specDisplay.m
+% 
+% 	Takes the excel file containing the sound data file (720 chunks of 256 bytes).
+% 	Create an array of the fft of the data.
+% 	Output a text file containing the normalized fft to be used as an input to the video display code.
+% 
+% */
 
 [num, ~, ~]= xlsread('pianoSample.csv');
 i=0;
 j=1;
 k=256;
-numRaw=num; // numRaw will be used to for the spectrogram in matLab to test
+numRaw=num; %// numRaw will be used to for the spectrogram in matLab to test
 while(i<720)
    num(j:k)=fft( num(j:k),256);
    j=j+256;
@@ -21,13 +21,13 @@ end
 num=abs(num);
 num=num/(max(num));
     
-file = fopen('Shashank.txt', 'w');
-i=0;
-while (i<(720*256))   
- 
-   fprintf(file,'%.6f,\n' ,num(i+1));
-   array(i+1)=num(i+1);
-   i=i+1;
-end
+% file = fopen('Shashank.txt', 'w');
+% i=0;
+% while (i<(720*256))   
+%  
+%    fprintf(file,'%.6f,\n' ,num(i+1));
+%    array(i+1)=num(i+1);
+%    i=i+1;
+% end
 
-spectrogram(numRaw,256,[],[],44100,'yaxis')
+spectrogram(numRaw,256,[],[],44100,'yaxis');
