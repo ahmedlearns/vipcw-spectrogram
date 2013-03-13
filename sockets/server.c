d9aed2172d97f8a6257bcb45b620db3972f4a3c8
Ahmed Ismail <meedowi@gmail.com>
2013-03-13 16:17:49 -0400

    Added server that expects a buffer

--- sockets/server.c
+++ sockets/server.c
@@ -1,5 +1,4 @@
-/* A simple server in the internet domain using TCP
-   The port number is passed as an argument */
+/* A simple server in the internet domain using TCP   The port number is passed as an argument */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,13 +6,11 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
-
 void error(const char *msg)
 {
     perror(msg);
     exit(1);
 }
-
 int main(int argc, char *argv[])
 {
      int sockfd, newsockfd, portno;
@@ -21,35 +18,39 @@ int main(int argc, char *argv[])
      char buffer[256];
      struct sockaddr_in serv_addr, cli_addr;
      int n;
-     if (argc < 2) {
+     if (argc < 2)
+ {
          fprintf(stderr,"ERROR, no port provided\n");
          exit(1);
      }
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
-        error("ERROR opening socket");
+     if (sockfd < 0)
+         error("ERROR opening socket");
      bzero((char *) &serv_addr, sizeof(serv_addr));
      portno = atoi(argv[1]);
      serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
+     serv_addr.sin_addr.s_addr = INADDR_ANY; 
+    serv_addr.sin_port = htons(portno);
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,              sizeof(serv_addr)) < 0)
+               error("ERROR on binding");
      listen(sockfd,5);
      clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, 
-                 (struct sockaddr *) &cli_addr, 
-                 &clilen);
-     if (newsockfd < 0) 
-          error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
+     newsockfd = accept(sockfd,                  (struct sockaddr *) &cli_addr,                  &clilen);
+     if (newsockfd < 0)
+           error("ERROR on accept"); 
+    bzero(buffer,256); 
+    n = read(newsockfd,buffer,255); 
+    if (n < 0)
+ error("ERROR reading from socket");
+     //printf("Here is the message: %s\n",buffer);
+//////////////////////////////////////////////////////////////////
+	FILE * f =fopen("data.txt","w");
+	fwrite(buffer,1,255,f);
+	fclose(f);
+/////////////////////////////////////////////////////////////////
      n = write(newsockfd,"I got your message",18);
      if (n < 0) error("ERROR writing to socket");
      close(newsockfd);
      close(sockfd);
-     return 0; 
-}
+     return 0;
+ }
