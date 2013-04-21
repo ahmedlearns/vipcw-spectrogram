fba41d52325a22d1df44c33c97a56b9b6965eeec
Ahmed Ismail <ahmedismail91@gmail.com>
2013-04-21 13:11:02 -0700

    Updated Readme, gitignore

--- fft/monofft.c
+++ fft/monofft.c
@@ -18,22 +18,13 @@
 #include "fft.h"
 
 
-int main(int argc, char** argv)
+void genfft(float* fbuffer, int N)
 {
-    float fbuffer[128];
-
-    // Read Mic Data
-    float wavBuffer[128];
-    int pipe;
-    mkfifo("wavePipe", S_IRWXO);
-    system("arecord -d1 -r4000 -D hw:CARD=AudioPCI, DEV=0 wavePipe");
-    pipe = open("wavePipe", O_WRONLY);
-    read(pipe, wavBuffer, sizeof(float)*128);
-
-
-    int i=0,j=0, k=0, l=0;
-    int N = 128;                    /* number of points in FFT */
-    double (*X)[2];          		 /* pointer to frequency-domain samples */   /* double */
+    int i=0,j,k,l=0;
+    // int N = 256;                    /* number of points in FFT */
+    // float fbuffer[N];
+    int wavbuffer[N];
+    double (*X)[2];                  /* pointer to frequency-domain samples */   /* double */
     double x[N][2];             /* double */
     double mag, min, max, mag_norm;                 /* double */
     X = malloc(2 * N * sizeof(double));  /* double */
@@ -41,50 +32,51 @@ int main(int argc, char** argv)
         x[i][1]=0;
         i++;
     }
-
-    // int lSize;
-    // FILE * f = fopen("wavePipe", "r"); //opening the 2 channels wave file
-    //FILE * f1 = fopen ("pianoMono.wav", "wb"); // this is  the file where the one channel sound file will be saved
-    //FILE * f2 = fopen ("pianoFFT.txt", "w"); // a text file that will have the left channel sound data,
+    // mkfifo("wavePipe", S_IRWXO);
+    // system("arecord -d1 -r4000 -D plughw:CARD=U0x46d0x80a,DEV=0  wavePipe");
+    int lSize;
+    //FILE * f = fopen("chirp10-500Hz.wav", "r"); //opening the 2 channels wave file
+    FILE * f = fopen("wavePipe.wav", "r"); //opening the 2 channels wave file
+    if(!f) printf("Error readinf from wavePipe");
+    // FILE * f2 = fopen ("chirpFFT.txt", "w"); // a text file that will have the left channel sound data,
     //just to see what we're dealing with
-   // FILE * f3 = fopen ("pianoSample.csv", "w");  // an excel file that have the data to use in matLab
-    // fseek (f , 0 , SEEK_END);
-    // lSize = ftell (f);
-    // rewind (f);
+    fseek (f , 0 , SEEK_END);
+    lSize = ftell (f);
+    rewind (f);
 
-    // typedef struct wave {
-    //     int chunkID;
-    //     int chunkSize;
-    //     int format;
-    //     int subChunk1ID;
-    //     int subChunk1size;
-    //     int audio_num_ch;
-    //     int sampleRate;
-    //     int byteRate;
-    //     int bckAli_bitPs;
-    //     int subChunk2ID;
-    //     int subChunk2size;
-    //     int  data[(lSize-40)/4];
-    // } wave;
-    // wave* mywave;
-    // mywave = (wave *) malloc( lSize); 
- 
-    // fread(mywave,1,lSize,f);
+    typedef struct wave {
+        int chunkID;
+        int chunkSize;
+        int format;
+        int subChunk1ID;
+        int subChunk1size;
+        int audio_num_ch;
+        int sampleRate;
+        int byteRate;
+        int bckAli_bitPs;
+        int subChunk2ID;
+        int subChunk2size;
+        int  data[(lSize-40)/4];
+    } wave;
+    wave * mywave;
+    mywave=(wave *) malloc( lSize);
 
+    fread(mywave,1,lSize,f);
+    // printf("size ............. %d\n",lSize);
+    // printf("size ......double....... %lu\n",(sizeof(double)*128));
+    // printf("size ......double....... %lu\n",(sizeof(float)*128));
 
-    
+    i=0,j=0;
+    k=0;
     int mask=65535;
     int  leftVa;
-    // while (i<((lSize-40)/4)) {
-    while (i < (sizeof(wavBuffer) / sizeof(float))) {
+    while (i<((lSize-40)/4)) {
 
-        // leftVa = (wavBuffer[i+1]<<16) | (wavBuffer[i] & mask);
-        // wavBuffer[j] = leftVa;
+        leftVa=(mywave->data[i+1]<<16) | (mywave->data[i] & mask);
+        mywave->data[j]=leftVa;
 
-        // leftVa = (mywave->data[i+1]<<16) | (mywave->data[i] & mask);
-        // mywave->data[j]=leftVa;
-
-        x[k][0] = wavBuffer[k];
+        x[k][0]=leftVa;
+        wavbuffer[k] = leftVa;
         j++;
         k++;
         //~ printf("k is %d and N is %d\n", k, N);
@@ -107,32 +99,30 @@ int main(int argc, char** argv)
                 mag=sqrt((X[l][0]*X[l][0])+(X[l][1]*X[l][1]));
 
                 mag_norm = (mag - min)/(max - min);
-
                 fbuffer[l] = mag_norm;
-                // fprintf(f2,"%12f,\n", mag_norm);
-                // fprintf(f3,"%12f,\n", mag_norm);
+                //printf("%12f\n", mag_norm);
+               //fprintf(f2,"%12f\n", mag_norm);
+                //fprintf(f3,"%12f,\n", mag_norm);
             }
             k=0;
         }
-        // i=i+2;
-        i++;
+        i=i+2;
     }
-
-    for(j = 0; j < 128; j++)
-        printf("wavBuffer[%d] is %f, fbuffer[%d] is %f\n", j, wavBuffer[j], j, fbuffer[j]);
 
     // mywave->audio_num_ch=mywave->audio_num_ch-65536;
     // mywave->chunkSize=(mywave->chunkSize/2)+18;
     // mywave->byteRate=mywave->byteRate/2;
     // mywave->bckAli_bitPs=mywave->bckAli_bitPs-2;
-    // mywave->subChunk2size=mywave->chunkSize-36;
+    // mywave->subChunk2size=mywave->chunkSize-36; 
+    
+    for(i = 0; i < N; i++)
+        printf("wavbuffer[%d] is %12d, fbuffer[%d] is %12f\n", i, wavbuffer[i], i, fbuffer[i]);
 
 
-    // fwrite(mywave,1,(lSize/2)+22,f1);
-    // fclose (f);
-    // fclose (f1);
+   // fwrite(mywave,1,(lSize/2)+22,f1);
+    fclose (f);
     // fclose (f2);
-    // fclose (f3);
     free(X);
-    return 0;
+    return fbuffer;
 }
+
---
+++ fft/monofft.h
@@ -0,0 +1,5 @@
+/**
+ * monofft.h
+ */
+
+float* genfft(int);
